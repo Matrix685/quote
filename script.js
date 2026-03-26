ad6af7b030f728008c9ad9db1a22be326b121a73
matrix685 <oflynnc48len@gmail.com>
2026-03-26 10:27:49 +0000

    broken but mostly fine

--- script.js
+++ script.js
@@ -1,80 +1,95 @@
 const main = document.getElementById("main");
+// const altolympus = document.getElementById("altolympus");
+// const misc = document.getElementById("misc");
+const carousels = document.querySelectorAll(".carousel");
 
 let quotes = [
-    {
-        id: "spider",
-        image: "https://lh3.googleusercontent.com/proxy/mtBzXWcpqeV9IGsqzM9f4KVmPa8eukHxzdQlQxK2mPDV4vunA14hWOy1JbSw1zgFSm-QzW0GUAGLUXsNRTYyGtLeVgQpGh2bGLSRYdONBntYJhi6WL21Zq6GIK6Gufh0IL8WeU4sNIYvQ8C6B-ycto_WQA",
-        quote: '"With great power comes great responsibility" - Ben Parker',
-    },
-    {
-        id: "owl",
-        image: "https://spotlight153.com/wp-content/uploads/2022/06/62251732-0-q80-900x506.jpg",
-        quote: '"Now I am both King and Queen, best of both things" - King',
-    },
-    {
-        id: "amphibia",
-        image: "https://upload.wikimedia.org/wikipedia/en/4/45/Amphibia_Season_3_Intro_Image.jpg",
-        quote: '"Spranne against the world!"- Sprig and Anne<br />(while against the world)',
-    },
-    {
-        id: "history",
-        image: "https://i.ytimg.com/vi/xuCn8ux2gbs/maxresdefault.jpg",
-        quote: '"Those are the Tamil Kings, no one conquers the Tamil Kings" - Bill Wurtz',
-    },
-    {
-        id: "arcane",
-        image: "https://cdn.europosters.eu/image/1300/254049.jpg",
-        quote: '"In our pursuit of greatness, we failed to do good" - Viktor',
-    },
-    {
-        id: "altolympus",
-        image: "https://www.ancientworldmagazine.com/site/assets/files/1195/rubens-phaeton-1.jpg",
-        quote: `"You can't just <i>SAY</i> perchance" - altolympus`,
-    },
-    {
-        id: "undertale",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzjmDRF7gS0Jt3XBMQ6PUm5WsVJdc1G0JBw&s",
-        quote: `"Despite everything, it's still you" - Asgore's mirror`,
-    },
-    {
-        id: "sans",
-        image: "https://cdn.mos.cms.futurecdn.net/5nULLkfBtxNpjR5SmvmSw-1200-80.jpg",
-        quote: `"my brother has a very special attack" - sans`,
-    },
-    {
-        id: "aurora",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpTBpnkwrN-hu9sgXK1KNhVYNZAwb_qXwHOQ&s",
-        quote: '"Nobody dies. Not for me. Not ever" - Kendal',
-    },
-    {
-        id: "hades",
-        image: "https://m.media-amazon.com/images/M/MV5BYjBlODg3ZTgtN2ViNS00MDlmLWIyMTctZmQ2NWYwMzE2N2RmXkEyXkFqcGdeQVRoaXJkUGFydHlJbmdlc3Rpb25Xb3JrZmxvdw@@._V1_.jpg",
-        quote: '"There is no escape." - Hades (from Hades)',
-    },
-    {
-        id: "linux",
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRzrZVQJhpaQeiO4NYlGTNMKc26LHTA-o-RA&s",
-        quote: '"You should install a package about that" - altolympus',
-    },
-    {
-        id: "eda",
-        image: "https://d23.com/app/uploads/2021/06/1180w-600h-060921_The-Owl-House_season-two_iris.jpg",
-        quote: `"Quitting! It's like trying, but easier" - Eda the Owl Lady`,
-    },
-    {
-        id: "sky",
-        image: "https://blog.gale.com/wp-content/uploads/2024/07/iStock-1693812103.jpg",
-        quote: `"What's up?" - Anon,<br />"The sky" - altolympus`,
-    },
+    [
+        {
+            id: "altolympus",
+            image: "https://www.ancientworldmagazine.com/site/assets/files/1195/rubens-phaeton-1.jpg",
+            quote: `"You can't just <i>SAY</i> perchance" - altolympus`,
+        },
+        {
+            id: "linux",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRzrZVQJhpaQeiO4NYlGTNMKc26LHTA-o-RA&s",
+            quote: '"You should install a package about that" - altolympus',
+        },
+        {
+            id: "sky",
+            image: "https://blog.gale.com/wp-content/uploads/2024/07/iStock-1693812103.jpg",
+            quote: `"What's up?" - Anon,<br />"The sky" - altolympus`,
+        },
+    ],
+
+    [
+        {
+            id: "spider",
+            image: "https://lh3.googleusercontent.com/proxy/mtBzXWcpqeV9IGsqzM9f4KVmPa8eukHxzdQlQxK2mPDV4vunA14hWOy1JbSw1zgFSm-QzW0GUAGLUXsNRTYyGtLeVgQpGh2bGLSRYdONBntYJhi6WL21Zq6GIK6Gufh0IL8WeU4sNIYvQ8C6B-ycto_WQA",
+            quote: '"With great power comes great responsibility" - Ben Parker',
+        },
+        {
+            id: "owl",
+            image: "https://spotlight153.com/wp-content/uploads/2022/06/62251732-0-q80-900x506.jpg",
+            quote: '"Now I am both King and Queen, best of both things" - King',
+        },
+        {
+            id: "amphibia",
+            image: "https://upload.wikimedia.org/wikipedia/en/4/45/Amphibia_Season_3_Intro_Image.jpg",
+            quote: '"Spranne against the world!"- Sprig and Anne<br />(while against the world)',
+        },
+        {
+            id: "history",
+            image: "https://i.ytimg.com/vi/xuCn8ux2gbs/maxresdefault.jpg",
+            quote: '"Those are the Tamil Kings, no one conquers the Tamil Kings" - Bill Wurtz',
+        },
+        {
+            id: "arcane",
+            image: "https://cdn.europosters.eu/image/1300/254049.jpg",
+            quote: '"In our pursuit of greatness, we failed to do good" - Viktor',
+        },
+
+        {
+            id: "undertale",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyzjmDRF7gS0Jt3XBMQ6PUm5WsVJdc1G0JBw&s",
+            quote: `"Despite everything, it's still you" - Asgore's mirror`,
+        },
+        {
+            id: "sans",
+            image: "https://cdn.mos.cms.futurecdn.net/5nULLkfBtxNpjR5SmvmSw-1200-80.jpg",
+            quote: `"my brother has a very special attack" - sans`,
+        },
+        {
+            id: "aurora",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpTBpnkwrN-hu9sgXK1KNhVYNZAwb_qXwHOQ&s",
+            quote: '"Nobody dies. Not for me. Not ever" - Kendal',
+        },
+        {
+            id: "hades",
+            image: "https://m.media-amazon.com/images/M/MV5BYjBlODg3ZTgtN2ViNS00MDlmLWIyMTctZmQ2NWYwMzE2N2RmXkEyXkFqcGdeQVRoaXJkUGFydHlJbmdlc3Rpb25Xb3JrZmxvdw@@._V1_.jpg",
+            quote: '"There is no escape." - Hades (from Hades)',
+        },
+
+        {
+            id: "eda",
+            image: "https://d23.com/app/uploads/2021/06/1180w-600h-060921_The-Owl-House_season-two_iris.jpg",
+            quote: `"Quitting! It's like trying, but easier" - Eda the Owl Lady`,
+        },
+    ],
 ];
 
-for (let i = quotes.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    [quotes[i], quotes[j]] = [quotes[j], quotes[i]];
+function mix(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
 }
 
-for (const quote of quotes) {
-    main.innerHTML += `
+quotes.forEach((n) => mix(n));
+
+quotes.forEach((group, index) => {
+    for (const quote of group) {
+        carousels[index].innerHTML += `
         <div id=outer>
             <div class="quote">
                 <div class="container ${quote.id}">
@@ -84,7 +99,21 @@ for (const quote of quotes) {
             </div>
         </div>
     `;
-}
+    }
+});
+
+// for (const quote of quotes[1]) {
+//     misc.innerHTML += `
+//         <div id=outer>
+//             <div class="quote">
+//                 <div class="container ${quote.id}">
+//                     <div class="img" style="background-image: url(${quote.image})"></div>
+//                     <p class="caption">${quote.quote}</p>
+//                 </div>
+//             </div>
+//         </div>
+//     `;
+// }
 
 const outers = document.querySelectorAll("#outer");
 let timeouts = [];
@@ -168,7 +197,7 @@ document.onmousemove = (e) => {
             left: `${x}px`,
             top: `${y}px`,
         },
-        { duration: 100, fill: "forwards" }
+        { duration: 100, fill: "forwards" },
     );
 
     // zoomBox.style.translate = `${x}px 10%`;
@@ -213,7 +242,7 @@ function zoom(el) {
             duration: 400,
             easing: "cubic-bezier(0.165, 0.84, 0.44, 1)",
             fill: "forwards",
-        }
+        },
     );
 
     zoomBox.style.display = "block";
@@ -243,7 +272,7 @@ function unzoom() {
         {
             duration: 600,
             easing: "cubic-bezier(0.165, 0.84, 0.44, 1)",
-        }
+        },
     );
 
     setTimeout(() => (zoomout.innerHTML = ""), 550);
@@ -291,6 +320,21 @@ function resize() {
 
 resize();
 window.onresize = resize;
+
+const carouselSelectors = document.querySelectorAll("#carousel-selector > p");
+
+for (const selector of carouselSelectors) {
+    selector.onclick = () => {
+        carouselSelectors.forEach((n) => (n.dataset.active = "false"));
+        carousels.forEach((n) => {
+            if (n.dataset.active == "true") n.dataset.active = "false";
+        });
+
+        selector.dataset.active = "true";
+        carousels[selector.id].dataset.active = "true";
+        // console.log(selector.id);
+    };
+}
 
 // sometimes background tiles get smaller across screen in wave style
 // randomize buncha shit
